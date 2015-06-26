6c4786167cb0a38c78b1e7c2c00822dcf4c36e13
Katharina <katharina.bierwagen@cern.ch>
2015-06-26 17:33:09 +0200

    added additional histograms

--- interface/Object.hpp
+++ interface/Object.hpp
@@ -20,6 +20,8 @@ class Object
         virtual inline float E(){ return p4.E(); }
         virtual inline int   IsObject(){return 0;}
         virtual inline float DeltaR(Object &o){ return p4.DeltaR(o.GetP4()); }
+        virtual inline float DeltaEta(Object &o){ return fabs(p4.Eta()-(o.GetP4()).Eta()); }
+        virtual inline float InvMass(Object &o){ return (p4.operator+=(o.GetP4())).M(); }
         virtual inline TLorentzVector & GetP4(){ return p4;}
         virtual inline void SetP4(TLorentzVector &x){p4 = x;}
 };
