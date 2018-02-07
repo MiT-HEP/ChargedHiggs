b33602a0b06fa2a7d7ce99305d3a06fea183afa7
maria <dalfonso@cern.ch>
2018-02-07 11:01:52 +0100

    add dat dy_lowMass_ht

---
+++ dat/catalog/2017.v23/dy_lowMass_ht.py
@@ -0,0 +1,7 @@
+sub=dalfonsoHiggs|/store/group/phys_higgs/HiggsExo/dalfonso/80x_MC_v2/v2.3/
+
+addfiles=%(dalfonsoHiggs)s/DYJetsToLL_M-5to50_HT-70to100_TuneCUETP8M1_13TeV-madgraphMLM-pythia8
+addfiles=%(dalfonsoHiggs)s/DYJetsToLL_M-5to50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8
+addfiles=%(dalfonsoHiggs)s/DYJetsToLL_M-5to50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8
+addfiles=%(dalfonsoHiggs)s/DYJetsToLL_M-5to50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8
+addfiles=%(dalfonsoHiggs)s/DYJetsToLL_M-5to50_HT-600toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8
