1b36e50f133435b904505066776aa0fc58321f95
Miao Hu <miao.hu@cern.ch>
2019-10-31 10:37:12 +0100

    add cutflow & basic (fat)jet properties

--- src/AnalysisVBShad.cpp
+++ src/AnalysisVBShad.cpp
@@ -36,9 +36,9 @@ void VBShadAnalysis::SetJetCuts(Jet *j) {
 }
 
 void VBShadAnalysis::SetFatJetCuts(FatJet *f){
-    f->SetEtaCut(2.4);
-    f->SetPtCut(450);
-    f->SetSDMassCut(65);
+    f->SetEtaCut(2.5);
+    f->SetPtCut(200);
+    f->SetSDMassCut(-1);
 }
 
 void VBShadAnalysis::Init(){
@@ -47,6 +47,20 @@ void VBShadAnalysis::Init(){
 	Log(__FUNCTION__,"INFO","Booking Histo Mass");
     for ( string l : AllLabel()  ) {
 	    Book ("VBShadAnalysis/Vars/Mmm_"+ l ,"Mmm;m^{#mu#mu} [GeV];Events", 360,60,150);
+
+        //cutflow
+        Book ("VBShadAnalysis/Cutflow_"+l, "cutflow; bit; Events", 12,0,12);
+        
+        //FatJet
+        Book ("VBShadAnalysis/Baseline/NFatJet_"+l, "NFatJet; NFatJet; Events", 5,0,5);
+        Book ("VBShadAnalysis/Baseline/pT_FatJet_"+l, "pT_FatJet; pT [GeV]; Events", 120,0,2400.);
+        Book ("VBShadAnalysis/Baseline/SDMass_FatJet_"+l, "SDMass_FatJet; SDMass [GeV]; Events", 100,0,200.);
+        Book ("VBShadAnalysis/Baseline/Tau21_FatJet_"+l, "Tau21_FatJet; tau21; Events", 50,0,1.0);
+
+        //Jet
+        Book ("VBShadAnalysis/Baseline/NJet_"+l, "NJet; FatJet; Events", 10,0,10);
+        Book ("VBShadAnalysis/Baseline/pT_Jet_"+l, "pT_Jet; pT [GeV]; Events", 80,0,1600.);
+
     } //end label loop
 
     if (VERBOSE)Log(__FUNCTION__,"DEBUG","End Init");
@@ -61,19 +75,29 @@ int VBShadAnalysis::analyze(Event *e, string systname)
     if (label == "Other") Log(__FUNCTION__,"WARNING","Unable to associate label to file: "+e->GetName() );
 
 
+    Fill("VBShadAnalysis/Cutflow_" +label, systname, 0, e->weight() );
+
+
     // TRIGGER STORY                                                                                                                                                                                             
-    bool passtrigger = (e->IsTriggered("HLT_PFHT800_v") || e->IsTriggered("HLT_AK8PFJet360_TrimMass30_v") || e->IsTriggered("HLT_AK8PFHT650_TrimR0p1PT0p3Mass50_v"));
+    bool passtrigger = (e->IsTriggered("HLT_PFHT_800_v") || e->IsTriggered("HLT_AK8PFJet360_TrimMass30_v") || e->IsTriggered("HLT_AK8PFHT650_TrimR0p1PT0p3Mass50_v"));
     if(!passtrigger) return EVENT_NOT_USED;
+
+    Fill("VBShadAnalysis/Cutflow_" +label, systname, 1, e->weight() );  //1--trigger
 
     // kill Top/W/Z 
     if ( e->Nleps() > 0 ) return EVENT_NOT_USED;
 
+    Fill("VBShadAnalysis/Cutflow_" +label, systname, 2, e->weight() );  //2--lep veto
+
     // events with MET in another category
     if ( e->GetMet().Pt() > 200 ) return EVENT_NOT_USED;
+
+    Fill("VBShadAnalysis/Cutflow_" +label, systname, 3, e->weight() );  //3--veto MET
 
     // events with B in another category   
     if ( e->Bjets() > 0 ) return EVENT_NOT_USED;
 
+    Fill("VBShadAnalysis/Cutflow_" +label, systname, 4, e->weight() );  //4--veto b
 
     //$$$$$$$$$
     //$$$$$$$$$
@@ -93,16 +117,67 @@ int VBShadAnalysis::analyze(Event *e, string systname)
             //            jets.push_back( j->GetP4() ) ;
         }
 
+    //AK8 jet
+    selectedFatJets.clear();
+    for(unsigned i=0;i<e->NFatJets() ; ++i)
+    {
+        FatJet *f=e->GetFatJet(i);
+
+        Fill("VBShadAnalysis/Baseline/SDMass_FatJet_" +label, systname, f->SDMass(), e->weight() );
+        Fill("VBShadAnalysis/Baseline/pT_FatJet_" +label, systname, f->Pt(), e->weight() );
+        Fill("VBShadAnalysis/Baseline/Tau21_FatJet_" +label, systname, f->Tau2()/f->Tau1(), e->weight() );
+
+        if(f->SDMass() < 65 || f->SDMass() > 105) continue;
+        if(f->Tau2() > f->Tau1() * 0.45) continue;
+
+        selectedFatJets.push_back(f);
+    }
+
+    Fill("VBShadAnalysis/Baseline/NFatJet_" +label, systname, selectedFatJets.size(), e->weight() );
+
+    if(selectedFatJets.size()<2) return EVENT_NOT_USED;
+
+    Fill("VBShadAnalysis/Cutflow_" +label, systname, 5, e->weight() );  //NFatjet cut
+
+    if( selectedFatJets[0]->InvMass(selectedFatJets[1]) < 1050 ) return EVENT_NOT_USED; 
+
+    Fill("VBShadAnalysis/Cutflow_" +label, systname, 6, e->weight() );  //InvMFatjet cut
+
+    //AK4
     selectedJets.clear();
     for(unsigned i=0;i<e->Njets() ; ++i)
     {
-        selectedJets.push_back(e->GetJet(i));
+        Jet *j=e->GetJet(i);
+
+        Fill("VBShadAnalysis/Baseline/pT_Jet_" +label, systname, j->Pt(), e->weight() );
+
+        if( j->DeltaR(selectedFatJets[0]) < 1.2 ) continue;
+        if( j->DeltaR(selectedFatJets[1]) < 1.2 ) continue;
+
+        selectedJets.push_back(j);
     }
 
+    Fill("VBShadAnalysis/Baseline/NJet_" +label, systname, selectedJets.size(), e->weight() );
+
+    if( selectedJets.size() < 2 ) return EVENT_NOT_USED;
+
+    Fill("VBShadAnalysis/Cutflow_" +label, systname, 7, e->weight() );  //NJet cut
+
+    if( selectedJets[0]->Eta() * selectedJets[1]->Eta() >=0 ) return EVENT_NOT_USED;
+
+    Fill("VBShadAnalysis/Cutflow_" +label, systname, 8, e->weight() );  //Jet seperate cut
+
+    if( fabs(selectedJets[0]->DeltaEta(selectedJets[1]) < 3.0 ) ) return EVENT_NOT_USED;
+
+    Fill("VBShadAnalysis/Cutflow_" +label, systname, 9, e->weight() );  //Jet separate cut
+
+    if( selectedJets[0]->InvMass(selectedJets[1]) < 1000 ) return EVENT_NOT_USED; 
+
+    Fill("VBShadAnalysis/Cutflow_" +label, systname, 10, e->weight() ); //InvMjet cut
     
-    for(unsigned i=0;i<selectedJets.size() ;++i) {
+    //for(unsigned i=0;i<selectedJets.size() ;++i) {
         //                if (selectedJets[i]->IsBJet() and selectedJets[i]->Pt() >30 and abs(selectedJets[i]->Eta())<2.4)  nbjets +=1;
-    }
+    //}
 
     if (VERBOSE)Log(__FUNCTION__,"DEBUG","end Analyze");
     return 0;
