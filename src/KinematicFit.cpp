ef5e08e1e0a1ba3e887fae6fc4ad54e102e2edb8
Andrea Carlo Marini <andrea.marini@cern.ch>
2019-11-20 15:51:38 +0100

    kinematic fit with simplex

--- src/KinematicFit.cpp
+++ src/KinematicFit.cpp
@@ -353,7 +353,7 @@ void KinematicFit::runGeneric(){
         alpha.reserve(p4.size());
         for(int i=0;i<p4.size();++i)
         {
-            float ai= gsl_vector_get (s->x, i);
+            double ai= gsl_vector_get (s->x, i);
             alpha.push_back(ai);
 
         }
@@ -397,3 +397,271 @@ void KinematicFit::print()
 
     std::cout<<" ---------------------------------------------"<<std::endl;
 }
+
+// ------------------------------------- KINEMATIC FIT 2 -------------------
+//
+
+#ifdef KF2
+class info_kf2{
+    // this class contains the parameters passed to func_kf2
+    // and similarly
+    public:
+        TLorentzVector p4;
+        float sigma;
+        int isV{0}; // consider this for V calculation
+
+        // I put this as static to link them to the parameters
+        static double lambda_MET;
+        static double lambda_V;
+        static double lambda_mqq;
+        static double mW;
+        static double mZ;
+        static int doV;//0 = no, 1=Z, 2=W
+
+};
+
+double info_kf2::lambda_MET=1.;
+double info_kf2::lambda_V=1.;
+double info_kf2::lambda_mqq=1000.;
+double info_kf2::mW=80.399;
+double info_kf2::mZ=91.1876;
+int info_kf2::doV=1;
+
+double func_kf2 (const gsl_vector * a, void * params)
+{
+    std::vector<info_kf2> *infos = (std::vector<info_kf2>*)params;
+    int N=infos->size();
+    double value=0;
+
+    double metx=0.,mety=0.;
+
+    TLorentzVector V;
+    TLorentzVector QQ;
+
+    for(unsigned i=0;i<N;++i)
+    {
+        double ai= gsl_vector_get(a,i);
+        double lai= std::log(ai);
+        double si = infos->at(i).sigma;
+
+        value+= std::pow( lai/si,2);
+        metx += ai*infos->at(i).p4.Px();
+        mety += ai*infos->at(i).p4.Py();
+
+        if (infos->at(i).isV)
+        {
+            V+=ai*infos->at(i).p4;
+        }
+    }
+
+    std::cout<<"Debug: iteration| stat: "<<value;
+    
+    double lambda_MET=infos->at(0).lambda_MET;
+    value += lambda_MET * std::pow(metx,2) + lambda_MET*std::pow(mety,2);
+
+    std::cout<<"| met: "<<metx<<" "<<mety<<" value="<<value;
+
+    double lambda_V=infos->at(0).lambda_V;
+    if (infos->at(0).doV==1) value += lambda_V*std::pow(V.M()-infos->at(0).mZ,2);
+    if (infos->at(0).doV==2) value += lambda_V*std::pow(V.M()-infos->at(0).mW,2);
+
+    std::cout<<"|V value="<<value<<std::endl;
+    
+    return value;
+} // function
+
+
+#include "Math/Minimizer.h"
+#include "Math/Factory.h"
+#include "Math/Functor.h"
+#include "TRandom2.h"
+#include "TError.h"
+
+void KinematicFit2::run_single()
+{
+    //prepare
+        std::vector<info_kf2> myinfos;
+        for(int i=0;i<p4.size();++i)
+        {
+            info_kf2 a;
+            a.p4=p4[i];
+            a.sigma=sigma[i];
+            a.isV=  (i==v1) or (i==v2);
+            myinfos.push_back(a);
+        }
+
+    //run gsl
+        gsl_multimin_function F;
+        F.n = p4.size();
+        F.f = &func_kf2;
+        F.params = &myinfos;
+        
+        gsl_vector *x = gsl_vector_alloc (p4.size());
+        // init 
+        for(unsigned i=0;i<p4.size();++i) gsl_vector_set(x,i,1.);
+
+        // minimizer
+        size_t iter = 0;
+        int status;
+        size_t size=0;
+
+        // step size 10%
+        gsl_vector *ss =  gsl_vector_alloc (p4.size());
+        gsl_vector_set_all (ss, 1.e-4);
+
+        const gsl_multimin_fminimizer_type *T;
+        gsl_multimin_fminimizer *s;
+
+        //T = gsl_multimin_fminimizer_nmsimplex2;
+        T = gsl_multimin_fminimizer_nmsimplex;
+        s = gsl_multimin_fminimizer_alloc (T, p4.size());
+
+        gsl_multimin_fminimizer_set (s, &F, x, ss); // first step-size and tolerance = decrease parameter
+
+        do
+        {
+            iter++;
+            status = gsl_multimin_fminimizer_iterate (s);
+
+            if (status) break; // exit on error
+
+            size = gsl_multimin_fminimizer_size (s);
+            status = gsl_multimin_test_size (size, 1e-10);
+            if (status == GSL_SUCCESS)
+            {
+                //printf ("converged to minimum at\n");
+                break;
+            }
+        }
+        while (status == GSL_CONTINUE && iter < maxIteration);
+
+        if (status != GSL_SUCCESS) { std::cout<<"[WARNING] Unable to find minimum"<<std::endl; }
+
+        //copy back alpha vector
+        alpha.clear();
+        alpha.reserve(p4.size());
+        for(int i=0;i<p4.size();++i)
+        {
+            double ai= gsl_vector_get (s->x, i);
+            alpha.push_back(ai);
+            if (debug>1) std::cout<<"** setting ai to be: "<<ai<<std::endl;
+
+        }
+        if(VERBOSE)std::cout<<"-->KF Run Generic: F2"<<std::endl;
+        value= func_kf2 (s->x,&myinfos) ;
+        
+        //dealloc
+        gsl_multimin_fminimizer_free (s);
+        gsl_vector_free (x);
+        gsl_vector_free (ss);
+
+        return;
+}
+
+void KinematicFit2::run(){
+    // reset
+    if (debug)std::cout<<"[KinematicFit2]::[run] Resetting output"<<std::endl;
+    value_out=1e10;
+    alpha_out.clear();
+    //
+
+    for (info_kf2::doV=1; info_kf2::doV<=2 ;++info_kf2::doV)
+    {
+    if (doV==1  and info_kf2::doV !=1) continue;
+    if (doV==2  and info_kf2::doV !=2) continue;
+
+    if (debug)std::cout<<"[KinematicFit2]::[run] Doing V:"<<info_kf2::doV<<std::endl;
+
+    for( size_t vv1=0; vv1<p4.size();++vv1)
+        for( size_t vv2=vv1+1;vv2<p4.size();++vv2)
+        {
+            v1 = vv1;
+            v2 = vv2;
+
+            if ( notV.size() >0 and (notV[vv1] or notV[vv2]))
+            {
+                if (debug)std::cout<<"[KinematicFit2]::[run] vv comb:"<<vv1<<","<<vv2<< "not allowed"<<std::endl;
+                continue;
+            }
+            if (debug)std::cout<<"[KinematicFit2]::[run] vv comb:"<<vv1<<","<<vv2<<std::endl;
+            TLorentzVector VV=p4[vv1]+p4[vv2];
+            if (cutWidth>0 and fabs(VV.M()- ((info_kf2::doV==1)?info_kf2::mZ:info_kf2::mW) )>cutWidth) {
+                if (debug)std::cout<<"[KinematicFit2]::[run] skipping combination for:"<<VV.M()<<"-"<<((info_kf2::doV==1)?info_kf2::mZ:info_kf2::mW)<< " cut width="<<cutWidth<<std::endl;
+                continue;
+            }
+            run_single(); // slow part
+            if (debug)std::cout<<"[KinematicFit2]::[run] run_single value="<<value<<std::endl;
+
+            for( size_t qq1=0; qq1<p4.size();++qq1)
+            {
+                if (qq1==vv1 or qq1==vv2) continue;
+                for( size_t qq2=qq1+1; qq2<p4.size();++qq2)
+                {
+                    if (qq2==vv1 or qq2==vv2) continue;
+                    TLorentzVector QQ =p4[qq1]+p4[qq2];
+                    double val2=value+lambda_mqq/QQ.M();
+                    if (debug)std::cout<<"[KinematicFit2]::[run] testing qq comb:"<<qq1<<","<<qq2<<" MQQ"<<QQ.M()<<std::endl;
+                    if (val2<value_out)
+                    {
+                        if (debug)std::cout<<"[KinematicFit2]::[run] resetting output: "<<val2<<" -> "<<value_out<<std::endl;
+                        value_out = val2;
+                        // copy alphas
+                        alpha_out.clear();
+                        for(auto x : alpha) alpha_out.push_back(x);
+                        V=info_kf2::doV;
+                        index_out.clear();
+                        index_out.push_back(vv1); 
+                        index_out.push_back(vv2); 
+                        index_out.push_back(qq1); 
+                        index_out.push_back(qq2); 
+                    }
+                }
+            }
+        }
+    }
+    return;
+}
+
+void KinematicFit2::print(){
+    std::cout<<"--- KINEMATIC FIT 2 ----"<<std::endl;
+    std::cout<<"-- Configuration "<<std::endl;
+    std::cout<<" * lambda_MET: "<<lambda_MET<<std::endl;
+    std::cout<<" * lambda_V: "<<lambda_V<<std::endl;
+    std::cout<<" * lambda_mqq: "<<lambda_mqq<<std::endl;
+    std::cout<<" * doV (1=Z,2=W,3=both): "<<doV<<std::endl;
+    std::cout<<" * maxIteration: "<<maxIteration<<std::endl;
+    std::cout<<" * cutWidth: "<<cutWidth<<std::endl;
+    std::cout<<"-- Input "<<std::endl;
+    for (size_t i=0;i<p4.size();++i)
+    {
+        std::cout<<" * p4 "
+            <<"pt="<<p4[i].Pt()<<", "
+            <<"eta="<<p4[i].Eta()<<", "
+            <<"phi="<<p4[i].Phi()<<", "
+            <<"m="<<p4[i].M()<<", "
+            <<"sigma="<<sigma[i]<<", "
+            <<"notV="<<((notV.size()>0)?notV[i]:-1)<<", "
+            <<std::endl;
+    }
+    std::cout<<"-- Output "<<std::endl;
+    std::cout<<" * value_out: "<<value_out<<std::endl;
+    std::cout<<" * V=(1=Z,2=W): "<<V<<std::endl;
+    for (size_t i=0;i<p4.size();++i)
+    {
+        std::cout<<" * alpha="
+            <<alpha_out[i]<<std::endl;
+    }
+    if (index_out.size()>1)
+    {
+        std::cout<< "* V="<<index_out[0]<<std::endl;
+        std::cout<< "* V="<<index_out[1]<<std::endl;
+    }
+    if (index_out.size()>3)
+    {
+        std::cout<< "* Q="<<index_out[2]<<std::endl;
+        std::cout<< "* Q="<<index_out[3]<<std::endl;
+    }
+    std::cout<<"------------------------"<<std::endl;
+}
+
+#endif
