5e044e977568a24838476dc25b24f5e342417fb3
Miao Hu <miao.hu@cern.ch>
2021-07-28 10:23:37 +0200

    Revert "datacards production"
    This reverts commit 78e1f6735bc3081439fcf64ff22d9e2159faa2c2.

--- script/bwsVBS.py
+++ script/bwsVBS.py
@@ -1,78 +1,21 @@
 import ROOT, os, sys, re
 from optparse import OptionParser
 from datetime import datetime
-import FwBinning as FwRebin
-
-
 
 if __name__=="__main__":
     usage='''%prog 
     '''
     parser=OptionParser(usage = usage)
+    #parser.add_option("-i","--input",help="input root file",default="/eos/user/d/dalfonso/VBS_BDT/FEB17")
     parser.add_option("-v","--verbose",type='int',help="verbose",default=1)
-
-    parser.add_option("-i","--input",type='string',help="input file name", default="HAD.root")
-
-    parser.add_option("-y","--year",type='int',help="year; full run2 == 2020",default=2016)
-    parser.add_option("-c","--category",type='string',help="5 category: BB, BMET, RMET, BBTAG, RBTAG", default="BB")
-    parser.add_option("-s","--analysisStra",type='string',help="which one to fit with:mVV, mjj, BDT, ... ? ", default="mjj")
-
-    parser.add_option("-q","--quote",type='int',help="sig ext.: 1: (WW/WZ/ZZ)+(ewk/qcd/int) all separate; 2: VVAll+(ewk/qcd/int); 3: (WW/WZ/ZZ)-All; 4: VV-All",default=1)
-
-    opt,args=parser.parse_args()
+    opts,args=parser.parse_args()
 
 
-lumis={
-    2016: 35.9,
-    2017: 41.5,
-    2018: 60.0,
-    2020: 137
-}
-
-
-## [ssww, osww, zzjj, wzbb, wznn]/[ewk,qcd,ewk-qcd] 
-xsecsig = [
-{"pro" : "ssWW", "cont": "EWK", "name": "WPMJJWPMJJjj_EWK_LO", "xsec" : 0.13},
-#{"pro" : "ssWW", "cont": "QCD", "name": "WPMJJWPMJJjj_QCD_LO", "xsec" : 0.11},
-#{"pro" : "ssWW", "cont": "INT", "name": "WPMJJWPMJJjj_EWK_QCD_LO", "xsec" : 0.24},
-{"pro" : "osWW", "cont": "EWK", "name": "WPJJWMJJjj_EWK_LO", "xsec" : 1.89},
-#{"pro" : "osWW", "cont": "QCD", "name": "WPJJWMJJjj_QCD_LO", "xsec" : 9.79},
-#{"pro" : "osWW", "cont": "INT", "name": "WPJJWMJJjj_EWK_QCD_LO", "xsec" : 11.65},
-{"pro" : "ZZ", "cont": "EWK", "name": "ZJJZJJjj_EWK_LO", "xsec" : 0.06},
-#{"pro" : "ZZ", "cont": "QCD", "name": "ZJJZJJjj_QCD_LO", "xsec" : 1.13},
-#{"pro" : "ZZ", "cont": "INT", "name": "ZJJZJJjj_EWK_QCD_LO", "xsec" : 1.19},
-{"pro" : "WZ", "cont": "EWK", "name": "ZBBWPMJJjj_EWK_LO", "xsec" : 0.13},
-#{"pro" : "WZ", "cont": "QCD", "name": "ZBBWPMJJjj_QCD_LO", "xsec" : 1.33},
-#{"pro" : "WZ", "cont": "INT", "name": "ZBBWPMJJjj_EWK_QCD_LO", "xsec" : 1.46},
-{"pro" : "WZ", "cont": "EWK", "name": "ZNuNuWPMJJjj_EWK_LO", "xsec" : 0.17}
-#{"pro" : "WZ", "cont": "QCD", "name": "ZNuNuWPMJJjj_QCD_LO", "xsec" : 1.78},
-#{"pro" : "WZ", "cont": "INT", "name": "ZNuNuWPMJJjj_EWK_QCD_LO", "xsec" : 1.95},
-]
-
-
-LikeBins=25
-likelihoodBinning = FwRebin.RebinLikelihood(LikeBins)
-
-
-
-def read_input():
-    psig = []
-
-    for sig in xsecsig:
-        if ("BB" in opt.category) and ("Btag" not in opt.category):
-            if ("WW" in sig['pro']) or ("ZJJZJJ" in sig['name']) or ("WJJZJJ" in sig['name']) :
-                psig.append(sig)
-        elif "MET" in opt.category:
-            if ("NuNu" in sig['name']):
-                psig.append(sig)
-        elif "Btag" in opt.category:
-            if ("BB" in sig['name']):
-                psig.append(sig)
-
-    return psig
+lumis=[]
+lumis.append(137)
 
 class DatacardBuilder:
-    def __init__(self,verbose=1):
+    def __init__(self,verbose=0):
         self.categories={}  # bin_name -> file, path, 
         self.processes={} #
         self.systs={}
@@ -124,7 +67,7 @@ class DatacardBuilder:
         if x not in self.systs:
             self.systs[x] = {
                 "syst": x, ## keep it also in the dictionary
-                'inname': [inname], #if shape input name
+                'innmae': [inname], #if shape input name
                 'cat-proc': [catproc],
                 'value': [value], # the list match the cat-proc regex above
                 'fname':[fname],
@@ -146,9 +89,9 @@ class DatacardBuilder:
         txt.write("%%%% Automatically created by %s\n"%sys.argv[0])
         txt.write("%%%% date %s\n"%(now.strftime("%Y/%m/%d %H:%M")))
 
-        txt.write("imax * number of channels\n")
-        txt.write("jmax * number of background minus 1\n")
-        txt.write("kmax * number of nuisance parameters\n")
+        txt.write("imax *\n")
+        txt.write("jmax *\n")
+        txt.write("kmax *\n")
 
         txt.write('-'*50+'\n')
         #shapes declaration
@@ -198,26 +141,16 @@ class DatacardBuilder:
         txt.write("* autoMCStats 0")
 
     def _manipulate_histo(self,htmp,corr ):
-        htmp.Scale(lumis[opt.year] * 1000 * corr)
-        #htmp.Rebin(2); ## REBIN --
+        htmp.Scale(lumis[0] * 1000 * corr)
+        htmp.Rebin(5); ## REBIN --
         return htmp
 
-    def _get_norm(self,hname):
-        for sig in xsecsig:
-            if hname in sig['name']:
-                return sig['xsec']
-        return 1.
-            
     def _get_histo(self,fname,hname,rename=""):
         if self.verbose >2: print "DEBUG","opening file:",fname
-        normalization = self._get_norm(hname)
-
-        if ("ZBBWPMJJjj" in hname):
-            fname="/eos/user/h/hum/VBSHad/SigV2jes_BBtag_2016_sig.root"
-        #if ("_BB_QCD_HT" in hname):
-        #    fname="FinalHisto/HADanti.root"
-        #    normalization = 0.0042
-
+        normalization = 1.
+        if ("_BB_QCD_HT" in hname):
+            fname="/eos/user/d/dalfonso/VBS_BDT/MARCH26/HADanti.root"
+            normalization = 0.0042
         fIn=ROOT.TFile.Open(fname)
         if self.fOut!=None: self.fOut.cd()
         if self.verbose >2: print "DEBUG","getting histo",hname,"->",rename
@@ -236,37 +169,6 @@ class DatacardBuilder:
         o.Write()
         self.garbage.append(o) ## avoid destruction
 
-    def play_binning(self):
-	h = None
-	hsig = None
-	for cat in self.categories:
-            d=self.categories[cat]
-            # proc
-            for proc in self.processes:
-                d2=self.processes[proc]
-                if cat not in d2['cat']:
-                    if self.verbose >2: print "DEBUG","excluding",cat,proc
-                    continue
-                for suffix in d2['suffix']:
-		    if not d2['issig']:
-                        if h==None:
-                            h=self._get_histo("%(path)s/%(fname)s"%d,"%(base)s_"%d+suffix,"%(cat)s_"%d+proc)
-                        else:
-                            hTmp=self._get_histo("%(path)s/%(fname)s"%d,"%(base)s_"%d+suffix,"")
-                            h.Add(hTmp)
-		    else:
-                        if hsig==None:
-                            hsig=self._get_histo("%(path)s/%(fname)s"%d,"%(base)s_"%d+suffix,"%(cat)s_"%d+proc)
-                        else:
-                            hsigTmp=self._get_histo("%(path)s/%(fname)s"%d,"%(base)s_"%d+suffix,"")
-                            hsig.Add(hsigTmp)
-
-        #### current bin strategy: sig_err/sig < 20%; bkg_err/bkg < 20%; num_bkg > 1
-        #### Not too aggresive: control on tot_bkg yields, but not on individual ones
-        mapping = likelihoodBinning.createMapping(h,hsig)
-        return mapping
-
-
     def write_inputs(self,outname): #datacard.txt
         outroot=re.sub(".txt.*",".inputs.root",outname)
         self.fOut=ROOT.TFile.Open(outroot,"RECREATE")
@@ -275,9 +177,6 @@ class DatacardBuilder:
 
         self._write(info)
 
-	LikelihoodMapping = self.play_binning()
-
-
         for cat in self.categories:
             d=self.categories[cat]
             # data
@@ -285,8 +184,6 @@ class DatacardBuilder:
             if data==None: 
                 print "[WARNING] Creating fake data histogram for",cat
                 data=ROOT.TH1D("%(cat)s_data_obs"%d,"fake data histogram with 100 bins",100,0,100)
-	    else:
-                data =  likelihoodBinning.applyMapping(LikelihoodMapping, data)
             self._write(data)
             # proc
             for proc in self.processes:
@@ -301,45 +198,28 @@ class DatacardBuilder:
                     else:
                         hTmp=self._get_histo("%(path)s/%(fname)s"%d,"%(base)s_"%d+suffix,"")
                         h.Add(hTmp)
-                h = likelihoodBinning.applyMapping(LikelihoodMapping, h)
                 self._write(h)
-
 
                 # SYST
                 for sname in self.systs:
                     s=self.systs[sname]
                     if 'shape' not in s['type']: continue
-
-                    # check if matched
-                    matched = False
-                    SystName = ''
-                    for (c,p),v,f,inh in zip(s['cat-proc'],s['value'],s['fname'],s['inname']) :
+                    # construct a list of all the matches, append -, and take the first match in each cat and proc
+                    # take all the matches
+                    matched=False
+                    for c,p,v,f,inh in zip(s['cat-proc'],s['value'],s['fname'],s['inname']) :
                         if re.match(c,cat) and re.match(p,proc) and v > 0:
-                            matched = True
-                            SystName = inh
-                    if not matched: continue
-
-                    hup = None
-                    hdn = None
-                    for suffix in d2['suffix']:
-                        if hup==None:
-                            hup=self._get_histo("%(path)s/%(fname)s"%d,"%(base)s_"%d+suffix+"_"+SystName+"Up","%(cat)s_"%d+proc+"_"+sname+"Up")
-                            hdn=self._get_histo("%(path)s/%(fname)s"%d,"%(base)s_"%d+suffix+"_"+SystName+"Down","%(cat)s_"%d+proc+"_"+sname+"Down")
-                        else:
-                            hupTmp=self._get_histo("%(path)s/%(fname)s"%d,"%(base)s_"%d+suffix+"_"+SystName+"Up","")
-                            hdnTmp=self._get_histo("%(path)s/%(fname)s"%d,"%(base)s_"%d+suffix+"_"+SystName+"Down","")
-                            hup.Add(hupTmp)
-                            hdn.Add(hdnTmp)
-                            #if matched: print "WARNING", "syst duplicate found","discarding",c,p,v,"matching for",cat,proc
+                            if matched: print "WARNING", "syst duplicate found","discarding",c,p,v,"matching for",cat,proc
+                            matched=True
+                            d['fname2']=fname
+                            d['inname']=inh
                             #$CHANNEL_$PROCESS_$SYSTEMATIC
-
-                    hup = likelihoodBinning.applyMapping(LikelihoodMapping, hup)
-                    hdn = likelihoodBinning.applyMapping(LikelihoodMapping, hdn)
-                 
-                    hup.Write()
-                    hdn.Write()
-                    self._write(hup)
-                    self._write(hdn)
+                            hup=self._get_histo("%(path)s/%(fname2)s"%d,"%(iname)Up"%d,"%(cat)s_"%d+proc+"_"+sname+"Up")
+                            hdn=self._get_histo("%(path)s/%(fname2)s"%d,"%(iname)Up"%d,"%(cat)s_"%d+proc+"_"+sname+"Down")
+                            hup.Write()
+                            hdn.Write()
+                            self._write(hup)
+                            self._write(hdn)
         self.fOut.Close()
         return self
                             
@@ -348,65 +228,63 @@ class DatacardBuilder:
 
 if __name__=="__main__":
 
-    sigprocess = read_input() 
+    ######################################
+    ###            nonRES              ###
+    ######################################
 
-    db=DatacardBuilder(opt.verbose)
+    db=DatacardBuilder(opts.verbose)
+    #def add_category(self,x,path, basename,obs_suffix,fname=opts.fname):
+    db.add_category("metB",'/eos/user/d/dalfonso/VBS_BDT/MARCH26','VBShadAnalysis/MVV_BMET','Data','MET.root')
+    db.add_category("metR",'/eos/user/d/dalfonso/VBS_BDT/MARCH26','VBShadAnalysis/MVV_RMET','Data','MET.root')
+    db.add_category("hadBB",'/eos/user/d/dalfonso/VBS_BDT/MARCH26','VBShadAnalysis/MVV_BB','Data','HAD.root')
+    db.add_category("hadRB",'/eos/user/d/dalfonso/VBS_BDT/MARCH26','VBShadAnalysis/MVV_RB','Data','HAD.root')
+    db.add_category("tagBB",'/eos/user/d/dalfonso/VBS_BDT/MARCH26','VBShadAnalysis/MVV_BBtag','Data','BHAD.root')
+    db.add_category("tagRB",'/eos/user/d/dalfonso/VBS_BDT/MARCH26','VBShadAnalysis/MVV_RBtag','Data','BHAD.root')
 
-    ## set categories
-    ## when no data, "data" can be substituted with any process, will not affect obtaining expected results
-    #db.add_category(opt.category,"/eos/user/h/hum/VBSHad","VBShadAnalysis/"+opt.analysisStra+"_"+opt.category,"Data",opt.input) 
-    db.add_category(opt.category,"/eos/user/h/hum/VBSHad","VBShadAnalysis/"+opt.analysisStra+"_"+opt.category,"QCD_HT",opt.input)
-
-    ## set bkg processes
-    #db.add_process('top',False,['TT_TuneCUETP8M2T4','ST','TTX','TTJets'],[opt.category])
-    #db.add_process('ttbar',False,['TTTo2L2Nu_TuneCP5','TTToSemiLeptonic_TuneCP5','TTToHadronic_TuneCP5','TT_Mtt-700to1000_TuneCP5','TT_Mtt-1000toInf_TuneCP5'],[opt.category])
-    db.add_process('ttbar',False,['TTTo2L2Nu_TuneCP5','TTToSemiLeptonic_TuneCP5','TTToHadronic_TuneCP5'],[opt.category])
-    #db.add_process('triBoson',False,['TRIBOSON'],[opt.category])
-    #db.add_process('diBoson',False,['DIBOSON'],[opt.category])
-    #db.add_process('Zinv',False,['ZJetsToNuNu'],[opt.category])
-    #db.add_process('Winv',False,['WJetsToLNu'],[opt.category])
-    if("MET" not in opt.category): db.add_process('QCD',False,['QCD_HT'],[opt.category])
-
-
-    ## set sig processes
-    for sig in sigprocess:
-        if opt.quote == 1:
-            if sig == sigprocess[0]: db.add_process(sig['pro']+sig['cont'],True,[sig['name']],[opt.category])
-            else: db.add_process(sig['pro']+sig['cont'],False,[sig['name']],[opt.category])
-        elif opt.quote == 2:
-	    if "EWK" in sig['cont']: db.add_process('VVEWK',True,[sig['name']],[opt.category])
-            else: db.add_process('VVOther',False,[sig['name']],[opt.category])
-        elif opt.quote == 3:
-            if sig['pro'] == sigprocess[0]['pro']: db.add_process(sig['pro'],True,[sig['name']],[opt.category])
-            else: db.add_process(sig['pro'],False,[sig['name']],[opt.category])
-        elif opt.quote == 4:
-            db.add_process('VV',True,[sig['name']],[opt.category])
-
-
-    ## set systs
+    #add_process(self,x,issig,suffix=[],cat=[]):
+    db.add_process('ttbar',False,['TT_TuneCUETP8M2T4','ST','TTX'],['metB','metR','hadBB','hadRB','tagBB','tagRB'])
+    db.add_process('VVV',False,['TRIBOSON'],['metB','metR','hadBB','hadRB','tagBB','tagRB'])
+    db.add_process('VV',False,['DIBOSON'],['metB','metR','hadBB','hadRB','tagBB','tagRB'])
+    db.add_process('QCD',False,['QCD_HT'],['metB','metR','hadBB','hadRB','tagBB','tagRB'])
+    db.add_process('Zinv',False,['ZJetsToNuNu'],['metB','metR','hadBB','hadRB','tagBB','tagRB'])
+    db.add_process('WPhadWPhad',True,['WPhadWPhadJJ_EWK_LO_SM_mjj100_pTj10_13TeV_madgraphMLM_pythia8'],['met','metR','hadBB','hadRB','tagBB','tagRB'])
+    db.add_process('ZnnZhad',True,['ZnnZhadJJ_EWK_LO_SM_mjj100_pTj10_13TeV_madgraphMLM_pythia8'],['met','metR','hadBB','hadRB','tagBB','tagRB'])
+    db.add_process('ZbbZhad',True,['ZbbZhadJJ_EWK_LO_SM_mjj100_pTj10_13TeV_madgraphMLM_pythia8'],['met','metR','hadBB','hadRB','tagBB','tagRB'])
+#    db.add_process('WWjj_SS_ll',True,['WWjj_SS_ll'],['met'])
+#    db.add_process('WWjj_SS_lt',True,['WWjj_SS_lt'],['met'])
+#    db.add_process('WWjj_SS_tt',True,['WWjj_SS_tt'],['met'])
     db.add_systematics('lumi','','lnN',('.*','.*'),1.025)
     db.add_systematics('QCDScale_ttbar','','lnN',('.*','ttbar'),1.05)
-    db.add_systematics('CMS_eff_trigger','','lnN',('.*','.*'),1.05)
-    db.add_systematics('CMS_QCDnonclosure_n','','lnN',('.*','QCD'),1.20)  ## QCD Norm
-    if ("BB" in opt.category) and ("Btag" not in opt.category): db.add_systematics('CMS_QCDnonclosure_s_BB','QCDNonclosure_BB','shape',('.*','QCD'),1.)  ## QCD shape
-    elif ("BBtag" in opt.category): db.add_systematics('CMS_QCDnonclosure_s_BBtag','QCDNonclosure_BBtag','shape',('.*','QCD'),1.)
-    db.add_systematics('CMS_pileUp','PU','shape',('.*','.*'),1.)
-    #db.add_systematics('CMS_scale_j','JES_Total','shape',('.*','.*'),1.)
-    ## break down JES sources
-    #db.add_systematics('jes_FlavorQCD','JES_FlavorQCD','shape',('.*','.*'),1.)
-    #db.add_systematics('jes_RelativeBal','JES_RelativeBal','shape',('.*','.*'),1.)
-    #db.add_systematics('jes_HF','JES_HF','shape',('.*','.*'),1.)
-    #db.add_systematics('jes_BBEC1','JES_BBEC1','shape',('.*','.*'),1.)
-    #db.add_systematics('jes_EC2','JES_EC2','shape',('.*','.*'),1.)
-    #db.add_systematics('jes_Absolute','JES_Absolute','shape',('.*','.*'),1.)
 
+
+    ######################################
+    ###            RES                 ###
+    ######################################
+
+    dbRES=DatacardBuilder(opts.verbose)
+    #def add_category(self,x,path, basename,obs_suffix,fname=opts.fname):
+    dbRES.add_category("hadBBRES",'/eos/user/d/dalfonso/AnalysisVBS/MARCH26/HAD','VBShadAnalysis/MVV_BB','Data','HAD.root')
+#    dbRES.add_category("hadBBRES",'/eos/user/d/dalfonso/AnalysisVBS/MARCH22/HAD','VBShadAnalysis/IN1500/MVV_BB','Data','HAD.root')
+#    dbRES.add_category("hadBBRES",'/eos/user/d/dalfonso/AnalysisVBS/MARCH22/HAD','VBShadAnalysis/IN1500/Mjj_BB','Data','HAD.root')
+
+    #add_process(self,x,issig,suffix=[],cat=[]):
+    dbRES.add_process('ttbar',False,['TT_TuneCUETP8M2T4','ST','TTX'],['hadBBRES'])
+#    dbRES.add_process('VVV',False,['TRIBOSON'],['hadBBRES'])
+#    dbRES.add_process('VV',False,['DIBOSON'],['hadBBRES'])
+    dbRES.add_process('QCD',False,['QCD_HT'],['hadBBRES'])
+    dbRES.add_process('HWW',True,['DoublyChargedHiggsGMmodel_HWW_M1500_13TeV-madgraph'],['hadBBRES'])
+
+    dbRES.add_systematics('lumi','','lnN',('.*','.*'),1.025)
+    dbRES.add_systematics('QCDScale_ttbar','','lnN',('.*','ttbar'),1.05)
 
     ######################################
     ###            WRITE               ###
     ######################################
 
-    db.write_cards('Datacards/NanoV2SDcorr/cms_vbshad_'+str(opt.year)+'_'+opt.analysisStra+'_'+opt.category+'.txt')
-    db.write_inputs('Datacards/NanoV2SDcorr/cms_vbshad_'+str(opt.year)+'_'+opt.analysisStra+'_'+opt.category+'.txt')
+#    db.write_cards('cms_vbs_hadr.txt')
+#    db.write_inputs('cms_vbs_hadr.txt')
 
+    dbRES.write_cards('cms_vbsRES_hadr.txt')
+    dbRES.write_inputs('cms_vbsRES_hadr.txt')
 
 
