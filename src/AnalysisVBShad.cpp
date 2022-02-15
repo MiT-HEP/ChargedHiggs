de4d48b043b1dde071d2e33f10ff65a3f49dfe5c
Miao Hu <miao.hu@cern.ch>
2022-02-15 08:27:29 +0100

    res tagger V2 (refine1: adjust match)

--- src/AnalysisVBShad.cpp
+++ src/AnalysisVBShad.cpp
@@ -4184,7 +4184,7 @@ int VBShadAnalysis::analyze(Event *e, string systname)
             float mWidthL = 15.;
             float mWidthH = 20.;
             double chi2Cut=6.;
-            float tmvacut = 0.65; // 80% signal
+            float tmvacut = 0.75; // 80% signal
             float kerascut = 0.5;
             //******************//
 
@@ -4385,7 +4385,7 @@ int VBShadAnalysis::analyze(Event *e, string systname)
             float mWidthL = 15.;
             float mWidthH = 20.;
             double chi2Cut=6.;
-            float tmvacut = 0.65; // 80% cut
+            float tmvacut = 0.75; // 80% cut
             float kerascut = 0.5;
             //******************//
 
